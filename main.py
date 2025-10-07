d7e27cb471ef3516651cae5ff50591b55bb8f543
designcraftsman <fzoussama25@gmail.com>
2025-10-07 21:57:43 +0100

    added the first function for file reading

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("hello world")
+def get_book_content(file_path):
+    if file_path == None:
+        return
+    with open(file_path) as f:
+        return f.read()
+
+def main():
+    book_text = get_book_content("books/frankenstein.txt")
+    print(book_text)
+
+main()
+
